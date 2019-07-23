0c9284e7dedef342c5e49370fd9de499d3b4fa11
dev-gdc <dev@gdcann.com>
2019-07-23 12:40:23 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+#Detect Motion for Raspberry Pi 3
+
+#How to push commit to GitHub:
+git add --all
+git commit -am "Comment Here."
+git remote add origin git@github.com:dev-gdc/MotionDetectPi3.git
+git remote set-url origin https://github.com/dev-gdc/MotionDetectPi3
+git push -u origin master
+
+#Password file
+You must create a dbpassword.config file with your database log on information.  I have provided an example password file (dbpassword.config.example) for your reference. Make sure you do not have spaces at the end of your strings in the config file!
+
---
+++ detectMotion.py
@@ -0,0 +1,48 @@
+import RPi.GPIO as GPIO
+import time
+import datetime
+import picamera
+import pymysql.cursors
+
+pirSensorPin = 18
+
+#Set up PIR sensor pin.
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pirSensorPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+#Set up camera
+camera = picamera.PiCamera()
+camera.hflip = True
+camera.vflip = True
+
+#Get Password information from config file.
+
+with open("dbpassword.config") as pwfile:
+	pwfilelist = pwfile.read().splitlines()
+
+hostf = pwfilelist[0]
+userf = pwfilelist[1]
+passwordf = pwfilelist[2]
+databasef = pwfilelist[3]
+tablef = pwfilelist[4]
+
+while 1:
+	GPIO.wait_for_edge(pirSensorPin, GPIO.RISING)
+	#print("Motion detected")
+	timeString = time.strftime("%Y%m%d-%H%M%S")
+	filename = ('/share/MotionDetectPi3/' + timeString + ".jpg")
+	camera.capture(filename)
+	sql = "INSERT INTO %s.%s (filename) VALUES ('%s')" % (databasef, tablef, (timeString + ".jpg"))
+	connection = pymysql.connect(host=hostf, user=userf, password=passwordf, db=databasef, charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+	try:
+		with connection.cursor() as cursor:
+			cursor.execute(sql)
+		connection.commit()
+	finally:
+		connection.close()
+
+
+
+
+		
+
